ae8ba14780394442fa383d4dbf8fdc7814ec733e
jys <95243456+dudtlr@users.noreply.github.com>
2022-11-23 04:23:37 +0900

    create (line by line draw)

--- JavaGameServer/src/ChatMsg.java
+++ JavaGameServer/src/ChatMsg.java
@@ -12,10 +12,22 @@ class ChatMsg implements Serializable {
 	public ImageIcon img;
 	public MouseEvent mouse_e;
 	public int pen_size; // pen size
+	private int x1, y1, x2, y2; //드래그시 선을 그을 좌표값
 
 	public ChatMsg(String UserName, String code, String msg) {
 		this.code = code;
 		this.UserName = UserName;
 		this.data = msg;
 	}
+	public void setX1(int x1){	this.x1 = x1;	}
+	public void setY1(int y1){	this.y1 = y1;	}
+	public void setX2(int x2){	this.x2 = x2;	}
+	public void setY2(int y2){	this.y2 = y2;	}
+
+	
+	public int getX1(){	return x1;	}
+	public int getY1(){	return y1;	}
+	public int getX2(){	return x2;	}
+	public int getY2(){	return y2;	}
+	
 }
